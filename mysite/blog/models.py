c7d1dfd3fcdcf81167ba528d6af52eca4ca5bd8c
suwito mulyadi <wito.forbes@gmail.com>
2024-03-27 07:48:00 +0000

    second commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
+# Add these:
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
 
-class HomePage(Page):
-    body = RichTextField(blank=True)
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body'),
+        FieldPanel('intro')
     ]
